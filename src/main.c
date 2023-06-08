768cb81e16ceb4d943bc5c096a2d49ccd99c82b2
Heroes Of Balkan <markomacura003@gmail.com>
2023-06-08 21:53:36 +0200

    Cleaning main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <errno.h>
-#include <unistd.h>
 
 #define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_NULL
 #define MA_DEBUG_OUTPUT
 #include "../libs/MiniAudio/miniaudio.h"
 
-int main(int argc, char *argv[]) {
+#include <gtk/gtk.h>
 
+#include "./gui/gui-main-window.c"
+
+int main(int argc, char *argv[]) {
+    int status;
     ma_result result;
     ma_engine engine;
     ma_sound sound;
-    if (argc < 2) {
-        perror("too few arguments\n");
-        return 1;
-    }
+    GtkApplication *userInterface;
 
     result = ma_engine_init(NULL, &engine);
     if (result != MA_SUCCESS) {
@@ -23,29 +23,12 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
-    // ma_engine_play_sound(&engine, argv[1], NULL);
-    result = ma_sound_init_from_file(&engine, argv[1], 0, NULL, NULL, &sound);
-    if (result != MA_SUCCESS) {
-        perror("Failed to read from file.");
-        printf("File: %s\n", argv[1]);
-        return result;
-    }
-    ma_sound_start(&sound);
+    userInterface = gtk_application_new("com.github.heroes_of_balkan.dancler", G_APPLICATION_REPLACE);
+    g_signal_connect(userInterface, "activate", G_CALLBACK(initialize_window_layout), NULL);
+    status = g_application_run(G_APPLICATION(userInterface), argc, argv);
+    g_object_unref(userInterface);
 
-    sleep(2);
-    ma_sound_stop(&sound);
-    printf("Press enter to continue.\n");
-    getchar();
-    
-    ma_sound_set_volume(&sound, 0.05);
-    ma_sound_seek_to_pcm_frame(&sound, 100000);
-    ma_sound_start(&sound);
 
-    printf("Press smth to quit lol.\n");
-    getchar();
-
-    ma_sound_stop(&sound);
-    ma_sound_uninit(&sound);
     ma_engine_uninit(&engine);
     
     return 0;
