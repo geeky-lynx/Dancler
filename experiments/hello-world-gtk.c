ea2ce8f766204f55942f8901d2f80f39b50d7186
Heroes Of Balkan <73647896+HeroesOfBalkan@users.noreply.github.com>
2023-09-30 22:24:19 +0200

    experimentation with callbacks

--- experiments/hello-world-gtk.c
+++ experiments/hello-world-gtk.c
@@ -1,20 +1,27 @@
+#include <stdio.h>
 #include <gtk/gtk.h>
-// Bamni Music
 
-static void print_hello(GtkWidget *widget, gpointer data) {
-  g_print("Hello World (Serbia)\n");
+gboolean hello(GtkWidget *widget, GdkEvent *event, gpointer data) {
+  printf("pointer in hello(): %p\n", data);
+  printf("data value: %d\n", *(int*)data);
 }
 
-static void activate(GtkApplication *app, gpointer user_data) {
+void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
   GtkWidget *button;
+
+  
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
   button = gtk_button_new_with_label("Hello World");
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+  // printf("pointer in activate(): %p\n", ptr);
+  // printf("abc value: %d\n", *(int*)ptr);
+  // Problem occurs here.
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(hello), user_data);
   gtk_window_set_child(GTK_WINDOW(window), button);
 
   gtk_window_present(GTK_WINDOW(window));
@@ -24,8 +31,14 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
+  gint abc = 5;
+  gpointer ptr = &abc;
+
+  printf("pointer in activate(): %p\n", ptr);
+  printf("abc value: %d\n", *(int*)ptr);
+
   app = gtk_application_new("org.gtk.example", G_APPLICATION_REPLACE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), ptr);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
