4021e5921c4e7f464f01896c9e641f326cd63627
HeroesOfBalkan <markomacura003@gmail.com>
2024-09-25 11:38:38 +0200

    Removing (commenting out) runtime logs in backend
    commenting out `printf()`-s

--- src/audio-controls/sound-control.c
+++ src/audio-controls/sound-control.c
@@ -43,6 +43,7 @@ int unitialize_backend(void) {
     if (isSoundInited)
         ma_sound_uninit(&sound);
     ma_data_source_uninit(&dataSource);
+    return 0;
 }
 
 
@@ -55,20 +56,21 @@ void set_volume_controls_in_percents(short volumePercertange) {
 
 
 int load_audio_to_queue(const char filename[]) {
-    printf("1\n");
+    // printf("1\n");
     // const ma_result QUEUE_RESULT = ma_resource_manager_data_source_init(queue, filename, 0, NULL, dataSource);
     // if (QUEUE_RESULT == MA_SUCCESS)
     //     isLoaded = true;
 
-    printf("2\n");
-    const ma_result SOUND_RESULT = ma_sound_init_from_file(&engine, filename, 0, NULL, NULL, &sound);// ma_sound_init_from_data_source(engine, dataSource, 0, NULL, sound);
+    // printf("2\n");
+    // ma_sound_init_from_data_source(engine, dataSource, 0, NULL, sound);
+    const ma_result SOUND_RESULT = ma_sound_init_from_file(&engine, filename, 0, NULL, NULL, &sound);
     if (SOUND_RESULT != MA_SUCCESS)
         return 0;
 
     isSoundInited = true;
-    printf("3\n");
+    // printf("3\n");
     // if (queueResult)
-    //     printf("Dancler Terminal Log: Audio file \"%s\" has been successfully added to queue.\n", filename);
+        // printf("Dancler Terminal Log: Audio file \"%s\" has been successfully added to queue.\n", filename);
     // else
     //     printf("Dancler Terminal Log: Audio file \"%s\" has failed adding to queue.\n", filename);
     // return (const int)QUEUE_RESULT;
@@ -79,30 +81,32 @@ int load_audio_to_queue(const char filename[]) {
 
 int unload_audio_from_queue(void) {
     ma_sound_uninit(&sound);
-    const ma_result QUEUE_RESULT = ma_resource_manager_data_source_uninit(&dataSource);
+    // const ma_result QUEUE_RESULT = ma_resource_manager_data_source_uninit(&dataSource);
+    constexpr int QUEUE_RESULT = MA_SUCCESS; // since resource manager isn't yet in use...
     // if (queueResult)
     //     printf("Dancler Terminal Log: Audio file has been removed from queue.\n");
     // else
     //     printf("Dancler Terminal Log: Audio file could not be removed from queue.\n"); // TODO: maybe change text bro
+    isSoundInited = false;
     return QUEUE_RESULT;
 }
 
 
 
 int toggle_playback(void) {
-    printf("isLoaded: %d\n", isLoaded);
+    // printf("isLoaded: %d\n", isLoaded);
     if (isLoaded)
         return -1;
 
     const ma_bool32 IS_PLAYING = ma_sound_is_playing(&sound);
-    printf("IS_PLAYING: %d\n", IS_PLAYING);
+    // printf("IS_PLAYING: %d\n", IS_PLAYING);
     if (IS_PLAYING) {
-        printf("da\n");
+        // printf("da\n");
         return ma_sound_stop(&sound);
         // printf("Dancler Terminal Log: Sound has stopped playing.\n");
     }
     else {
-        printf("ne\n");
+        // printf("ne\n");
         return ma_sound_start(&sound);
         // printf("Dancler Terminal Log: Sound has started playing.\n");
     }
