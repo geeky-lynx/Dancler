930f078ddad0cff45b53725ff910ff9770d80be3
HeroesOfBalkan <markomacura003@gmail.com>
2025-02-09 15:55:32 +0100

    Create function description for UI functions

--- src/gui/interface-api.h
+++ src/gui/interface-api.h
@@ -3,10 +3,32 @@
 
 #include "../header.h"
 
+/*
+    void initialize_window_layout(GtkApplication *application, gpointer userData): Creates user GUI
+    Parameters:
+        1. `GtkApplication *application`
+        2. `gpointer userData` (a.k.a. `void*`)
+    Returns: Nothing
+    Note: This function is only used once as a callback
+*/
 void initialize_window_layout(GtkApplication *application, gpointer userData);
 
-void create_playback_ui(GtkWidget *windowBox); // Forms information & controls for current playback
+/*
+    void create_playback_ui(GtkWidget *windowBox): Forms playback information & controls GUI elements
+    Parameters:
+        1. GtkWidget *windowBox
+            - GTK box (which is the "root" box) to append to
+    Returns: Nothing
+*/
+void create_playback_ui(GtkWidget *windowBox);
 
-void create_playlist_ui(GtkWidget *windowBox); // Forms playlist table (list of songs)
+/*
+    void create_playlist_ui(GtkWidget *windowBox): Forms playlist (list of songs) table GUI elements
+    Parameters:
+        1. GtkWidget *windowBox
+            - GTK box (which is the "root" box) to append to
+    Returns: Nothing
+*/
+void create_playlist_ui(GtkWidget *windowBox);
 
 #endif
